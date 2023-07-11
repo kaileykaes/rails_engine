dab42d158f38049c91d1b56a71d36789b26dfd18
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-11 17:58:57 -0600

    test: merchant show request

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -6,55 +6,70 @@ RSpec.describe 'Merchants API' do
   # end
 
   describe 'merchants endpoints' do
-    it 'sends a list of all merchants' do
-      create_list(:merchant, 4)
-      get '/api/v1/merchants'
+    describe 'index' do 
+      it 'sends a list of all merchants' do
+        create_list(:merchant, 4)
+        get '/api/v1/merchants'
 
-      expect(response).to be_successful 
+        expect(response).to be_successful 
 
-      merchants = JSON.parse(response.body, symbolize_names: true)
+        merchants = JSON.parse(response.body, symbolize_names: true)
 
-      expect(merchants.count).to eq(4)
+        expect(merchants.count).to eq(4)
 
-      merchants.each do |merchant|
-        expect(merchant).to have_key(:id)
-        expect(merchant[:id]).to be_an Integer
+        merchants.each do |merchant|
+          expect(merchant).to have_key(:id)
+          expect(merchant[:id]).to be_an Integer
 
-        expect(merchant).to have_key(:name)
-        expect(merchant[:name]).to be_a String
+          expect(merchant).to have_key(:name)
+          expect(merchant[:name]).to be_a String
+        end
+      end
+
+      it 'sends an empty array if no data' do 
+        get '/api/v1/merchants'
+        expect(response).to be_successful
+
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants.count).to eq(0)
+        expect(merchants.class).to be_an Array
+      end
+
+      it 'sends an array if one piece data' do 
+        create(:merchant)
+        get '/api/v1/merchants'
+
+        expect(response).to be_successful
+
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants.count).to eq(1)
+        expect(merchants.class).to be_an Array
+      end
+
+      it 'sends an array if no data' do 
+        get '/api/v1/merchants'
+        expect(response).to be_successful
+
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants.count).to eq(0)
+        expect(merchants.class).to be_an Array
       end
     end
 
-    it 'sends an empty array if no data' do 
-      get '/api/v1/merchants'
-      expect(response).to be_successful
+    describe 'show' do 
+      it 'sends a specific merchant' do 
+        merchant = create(:merchant)
 
-      merchants = JSON.parse(response.body, symbolize_names: true)
+        get "/api/v1/merchants/#{merchant.id}"
+        the_merchant = JSON.parse(response.body, symbolize_names: true)
 
-      expect(merchants.count).to eq(0)
-      expect(merchants.class).to be_an Array
-    end
-
-    it 'sends an array if one piece data' do 
-      create(:merchant)
-      get '/api/v1/merchants'
-
-      expect(response).to be_successful
-
-      merchants = JSON.parse(response.body, symbolize_names: true)
-
-      expect(merchants.count).to eq(1)
-      expect(merchants.class).to be_an Array
-    end
-
-    it 'sends an empty array if no data' do 
-      get '/api/v1/merchants'
-      expect(response).to be_successful
-
-      merchants = JSON.parse(response.body, symbolize_names: true)
-
-      expect(merchants.count).to eq(0)
-      expect(merchants.class).to be_an Array
+        expect(response).to be_successful
+        expect(the_merchant[:id]).to eq(merchant.id)
+        expect(the_merchant[:name]).to eq(merchant.name)
+      end
     end
   end
 end
