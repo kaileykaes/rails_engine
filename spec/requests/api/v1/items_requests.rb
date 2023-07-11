e4c7f1c6bcb9d84ec642710597f026171a2d9e7d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-11 17:14:14 -0600

    fix errors

--- spec/requests/api/v1/items_requests.rb
+++ spec/requests/api/v1/items_requests.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Items endpoints' do
-  # before(:each) do
-
-  # end
+  before(:each) do
+    @merchant = create(:merchant)
+  end
 
   describe 'relationships' do
-    it 'sends a list of all merchants' do
-      create_list(:item, 4)
-      get '/api/v1/merchants'
+    it 'sends a list of all items' do
+      create_list(:item, 4, merchant: @merchant)
+      get '/api/v1/items'
 
       expect(response).to be_successful 
 
@@ -36,7 +36,7 @@ RSpec.describe 'Items endpoints' do
     end
 
     it 'sends an array if one piece data' do 
-      create(:item)
+      create(:item, merchant: @merchant)
       get '/api/v1/items'
 
       expect(response).to be_successful
