c8a3ff9c174b4f49d2a52ac5de5a0dab3aa1c8c8
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-11 14:31:06 -0600

    add additional relationships

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,5 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
   has_many :transactions
+  has_many :invoice_items
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe Item, type: :model do
     it {should belong_to :merchant}
     it {should have_many :invoice_items}
     it {should have_many(:invoices).through(:invoice_items)}
+    it {should have_many(:transactions).through(:invoices)}
   end
 end
