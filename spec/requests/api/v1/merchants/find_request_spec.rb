415a6fa234df5af273b4307c106b16e2f8c59ca5
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-13 16:39:46 -0600

    test: search returns a single object, a single expected object; add body to merchant find index

--- spec/requests/api/v1/merchants/find_request_spec.rb
+++ spec/requests/api/v1/merchants/find_request_spec.rb
@@ -13,6 +13,13 @@ RSpec.describe 'Find a Merchant' do
       get '/api/v1/merchants/find', params: query_params
 
       expect(response).to be_successful
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      merchant = json[:data]
+
+      expect(merchant[:type]).to eq('merchant')
+      expect(merchant[:id]).to eq("#{@merchant.id}")
     end
   end
 end
