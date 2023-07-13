808957ebf24922c23f1954118f0308d321aaf170
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-12 19:19:58 -0600

    create item functionality

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
         resources :items, only: [:index], module: :merchants 
       end
       
-      resources :items, only: [:index, :show] do 
+      resources :items, only: [:index, :show, :create] do 
         resources :merchant, only: [:index], module: :items
       end
     end
--- spec/requests/api/v1/items_requests_spec.rb
+++ spec/requests/api/v1/items_requests_spec.rb
@@ -152,12 +152,15 @@ RSpec.describe 'Items endpoints' do
       })
       headers = {"CONTENT_TYPE" => "application/json"}
 
-      post "/api/v1/items"
+      post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
 
       created_item = Item.last
 
       expect(response).to be_successful
-      require 'pry'; binding.pry
+      expect(created_item.name).to eq(item_params[:name])
+      expect(created_item.description).to eq(item_params[:description])
+      expect(created_item.unit_price).to eq(item_params[:unit_price])
+      expect(created_item.merchant_id).to eq(item_params[:merchant_id])
     end
   end
 end
