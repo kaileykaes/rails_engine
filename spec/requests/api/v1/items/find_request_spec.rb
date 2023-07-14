208f026491ab0f526032ac353ad29bf1e1e03e44
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-13 18:29:41 -0600

    Test: route for find all items

---
+++ spec/requests/api/v1/items/find_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Find Items' do
+  before(:each) do
+    @merchant = create(:merchant)
+    create_list(:item, 8, merchant: @merchant)
+    @item_1 = create(:item, name: 'water bottle', merchant: @merchant)
+    @item_2 = create(:item, name: 'box', merchant: @merchant)
+    @item_2 = create(:item, name: 'Boston red sox hat', merchant: @merchant)
+  end
+
+  describe 'search for items' do
+    it 'returns items by name if found' do 
+      query_params = {
+        name: 'bo'
+      }
+
+      get '/api/v1/items/find_all', params: query_params
+
+      require 'pry'; binding.pry
+    end
+  end
+end
