8073f5c6f378ae31731d646dc0222af74a4ccb21
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-14 10:42:52 -0600

    test & create #find_by_price

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,19 @@ class Item < ApplicationRecord
     where("name ILIKE ?", "%#{query}%").order(:name)
   end
 
-  def self.max_price
-    maximum(:unit_price)
+  def self.find_by_price(min_price: nil, max_price: nil)
+    minimum = min_price.present? ? min_price : 0 
+    maximum = max_price.present? ? max_price : 2_000_000
+    where("unit_price BETWEEN #{minimum} AND #{maximum}")
   end
 
-  def self.min_price
-    minimum(:unit_price)
-  end
+  private
+
+  # def self.max_price
+  #   maximum(:unit_price)
+  # end
+
+  # def self.min_price
+  #   minimum(:unit_price)
+  # end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -39,25 +39,56 @@ RSpec.describe Item, type: :model do
       item_3 = create(:item, unit_price: 7.98, merchant: @merchant)
       item_4 = create(:item, unit_price: 8.20, merchant: @merchant)
 
-      # query = 
+      # query = {max_price: 8.00}
+
+      expect(Item.find_by_price(max_price: 8.00)).to include(item_1)
+      expect(Item.find_by_price(max_price: 8.00)).to include(item_2)
+      expect(Item.find_by_price(max_price: 8.00)).to include(item_3)
+      expect(Item.find_by_price(max_price: 8.00)).to_not include(item_4)
     end
 
-    it '#max_price' do 
+    it 'retrieves items based on min_price' do 
       item_1 = create(:item, unit_price: 3.45, merchant: @merchant)
       item_2 = create(:item, unit_price: 1.34, merchant: @merchant)
       item_3 = create(:item, unit_price: 7.98, merchant: @merchant)
       item_4 = create(:item, unit_price: 8.20, merchant: @merchant)
 
-      expect(Item.max_price).to eq(8.20)
+      # query = {min_price: 4.00}
+
+      expect(Item.find_by_price(min_price: 4.00)).to include(item_3)
+      expect(Item.find_by_price(min_price: 4.00)).to include(item_4)
+      expect(Item.find_by_price(min_price: 4.00)).to_not include(item_1)
+      expect(Item.find_by_price(min_price: 4.00)).to_not include(item_2)
     end
 
-    it '#min_price' do 
+    it 'retrieves items between prices' do 
       item_1 = create(:item, unit_price: 3.45, merchant: @merchant)
       item_2 = create(:item, unit_price: 1.34, merchant: @merchant)
       item_3 = create(:item, unit_price: 7.98, merchant: @merchant)
       item_4 = create(:item, unit_price: 8.20, merchant: @merchant)
 
-      expect(Item.min_price).to eq(1.34)
+      # query = {min_price: 4.00, max_price: 8.00}
+
+      expect(Item.find_by_price(min_price: 4.00, max_price: 8.00)).to include(item_3)
+      expect(Item.find_by_price(min_price: 4.00, max_price: 8.00)).to_not include(item_1)
+      expect(Item.find_by_price(min_price: 4.00, max_price: 8.00)).to_not include(item_2)
+      expect(Item.find_by_price(min_price: 4.00, max_price: 8.00)).to_not include(item_4)
     end
+
+    # it '#max_price' do 
+    #   item_1 = create(:item, unit_price: 3.45, merchant: @merchant)
+    #   item_2 = create(:item, unit_price: 1.34, merchant: @merchant)
+    #   item_3 = create(:item, unit_price: 8.20, merchant: @merchant)
+
+    #   expect(Item.max_price).to eq(8.20)
+    # end
+
+    # it '#min_price' do 
+    #   item_1 = create(:item, unit_price: 3.45, merchant: @merchant)
+    #   item_2 = create(:item, unit_price: 1.34, merchant: @merchant)
+    #   item_3 = create(:item, unit_price: 7.98, merchant: @merchant)
+      
+    #   expect(Item.min_price).to eq(1.34)
+    # end
   end
 end
